f42252a53a0fcf9ca65470027f893365f79264b9
Vladimir Lyz <LyzV@protonmail.com>
2019-06-21 12:34:59 +0300

    Добавил все файлы

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void hello(void)
+{
+	printf("Hello!\n");
+}
+
+void goodby(void)
+{
+	printf("Goodby...\n");
+}
+
+
---
+++ hello.h
@@ -0,0 +1,7 @@
+#ifndef _HELLO_H_
+#define _HELLO_H_
+
+extern void hello(void);
+extern void goodby(void);
+
+#endif//_HELLO_H_
